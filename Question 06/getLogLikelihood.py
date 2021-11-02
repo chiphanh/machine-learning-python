3f544f3a24754936e3140e4be563cac375b56840
pa-da-wan <73534577+pa-da-wan@users.noreply.github.com>
2021-11-03 00:35:22 +0100

    corrected getLogLikelihood

--- Question 06/getLogLikelihood.py
+++ Question 06/getLogLikelihood.py
@@ -15,18 +15,19 @@ def getLogLikelihood(means, weights, covariances, X):
     # logLikelihood  : log-likelihood
 
     #####Insert your code here for subtask 6a#####
-    N, D = shape(X)
-    K = shape(weights)[1]
+    N, D = X.shape
+    K = np.array(weights).reshape(1,-1).shape[1]
     logLikelihood =0
     for n in range(N):
         prob = 0
         for k in range(K):
             delta = X[n,:]-means[k]
             cov_mat = covariances[:,:,k]
-            cov_mat_inv = np.linalg.inv(cov_mat)
+            cov_mat_inv = np.linalg.pinv(cov_mat)
             denominator = (((2*np.pi)**(D/2))* np.sqrt(np.linalg.det(cov_mat)))
-            prob += weights[1,k]*np.exp(-0.5*(delta.T @ (cov_mat_inv @ delta)))/denominator
+            prob += np.array(weights).reshape(1,-1)[0,k]*np.exp(-0.5*(delta.T @ (cov_mat_inv @ delta)))/denominator
         logLikelihood += np.log(prob)
         
     return logLikelihood
+
 
